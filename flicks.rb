83bb3b649deb16d084854092c62a14f74c709767
Adam89 <Adamm1@aston.ac.uk>
2013-11-23 20:37:47 -0700

    added methods to my code

--- flicks.rb
+++ flicks.rb
@@ -1,6 +1,14 @@
-movie = "Goonies"
-puts "Mikey's favourite movie is #{movie}"
-rank = 10
+def weekday
+  current_time = Time.new
+  current_time.strftime("%A")
+end
 
-title = movie.capitalize.ljust(30,'.')
-puts "#{title} #{rank}"
+def movie_listing(title, rank=0)
+  "#{weekday.upcase}: #{title.capitalize} has a rank of #{rank}"
+end
+
+puts movie_listing("goonies", 10)
+puts movie_listing("Ghostbuster", 9)
+
+a_title = "goldfinger"
+puts movie_listing(a_title)
