6b0e27e2933f5777f233992e28571e84352f11b2
Adam89 <Adamm1@aston.ac.uk>
2013-11-26 20:55:25 -0700

    this is pretty cool now

--- playlist.rb
+++ playlist.rb
@@ -1,6 +1,6 @@
 require_relative 'movie'
 require_relative 'waldorf_and_statler'
-
+require_relative 'snack_bar'
 class Playlist
   attr_reader :name
   def initialize(name)
@@ -15,10 +15,20 @@ class Playlist
   def play(viewings)
     puts "#{name}'s playlist:"
     puts @movies.sort
+
+    snacks = SnackBar::SNACKS
+    puts "\nThere are #{snacks.count} snacks available in the snack bar"
+
+    snacks.each do |snack|
+      puts "#{snack.name} has #{snack.carbs} carbs"
+    end
+
     1.upto(viewings) do |count|
       puts "\nViewing #{count}"
       @movies.each do |movie|
         WaldorfAndStatler.review(movie)
+        snack = SnackBar.random
+        puts "#{movie.title} led to #{snack.carbs} #{snack.name} carbs being consumed "
       end
     end
   end
